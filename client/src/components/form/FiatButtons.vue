c630153939498ccc409e57e02af7ed7bac82ee12
Zane <alessandro.demaio@virgilio.it>
2023-08-02 19:13:53 +0200

    Bank Transfer View

---
+++ client/src/components/form/FiatButtons.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "FiatButtons"
+}
+</script>
+
+<template>
+    <input type="radio" class="btn-check" name="options-outlined" v-model="currency" value="EUR"
+        id="eur-currency-btn-success-outlined" autocomplete="off" checked>
+    <label class="btn btn-outline-primary" for="eur-currency-btn-success-outlined">Euro</label>
+
+    <input type="radio" class="btn-check" name="options-outlined" v-model="currency" value="USD"
+        id="usd-currency-check-2-outlined" autocomplete="off">
+    <label class="btn btn-outline-warning" for="usd-currency-check-2-outlined">Dollar</label>
+</template>
--- client/src/views/BankTransfer.vue
+++ client/src/views/BankTransfer.vue
@@ -1,10 +1,80 @@
 <template>
-  <div>BankTransfer</div>
+  <form class="deposit-form" @submit.prevent="onSubmit">
+    <div class="mb-3">
+      <input type="radio" class="btn-check" name="exchange-options-outlined" v-model="exchangeName" value="kraken"
+        id="kraken-dark-outlined" autocomplete="off" checked required>
+      <label class="btn btn-outline-dark" for="kraken-dark-outlined">Kraken</label>
+    </div>
+
+    <div class="mb-3">
+      <FiatButtons></FiatButtons>
+    </div>
+    <div class="mb-3">
+      <small id="emailHelp" class="form-text text-muted">This convert all USDT to {{ currency }} and transfers the funds
+        to
+        an international bank account.</small><br>
+      <button class="btn btn-outline-success" type="submit" :disabled="loading">
+        <span v-if="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        <span v-if="loading">Loading...</span><span v-else>Transfer</span>
+      </button>
+    </div>
+  </form>
+  <div v-if="loading" class="spinner-border text-success" role="status">
+    <span class="sr-only"></span>
+  </div>
+
+  <div v-if="error" class="error">{{ error }}</div>
+  <div>
+    {{ result }}
+  </div>
+
+  <div v-if="address" class="content">
+    <p>Balance: {{ balance }} {{ symbol }}</p>
+  </div>
 </template>
 
 <script>
-// TODO
+import FiatButtons from "@/components/form/FiatButtons.vue";
 export default {
   name: 'BankTransfer',
+  data: function () {
+    return {
+      loading: false,
+      currency: "EUR",
+      exchangeName: "kraken",
+      error: "",
+      balance: "",
+      result: "",
+    }
+  },
+  components: {
+    FiatButtons
+  },
+  methods: {
+    async onSubmit() {
+      // reset previous result
+      this.address = ""
+      this.balance = ""
+      this.loading = true
+
+      try {
+        const response = await this.$api.get('bank/transfer/currency')
+        console.log(response)
+        this.handleData(response)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        this.loading = false
+      }
+    },
+    handleData(result) {
+      const data = result.data
+      this.result = data
+      this.address = data.address
+      this.symbol = data.code
+      this.balance = data.balance
+      this.exchangeName = data.exchange
+    }
+  },
 }
 </script>
